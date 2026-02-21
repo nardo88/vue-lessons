6a1f6c5f02b4488b3bda6d3303d906f58633438a
nardo88 <nardo1988@mail.ru>
2026-02-21 18:58:19 +0300

    1

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,48 @@
 export default {
   data() {
     return {
-      total: 0, // Значение в состоянии
+      list: [
+        { _id: 1, title: 'title1', description: 'description 1' },
+        { _id: 2, title: 'title2', description: 'description 2' },
+        { _id: 3, title: 'title3', description: 'description 3' },
+        { _id: 4, title: 'title4', description: 'description 4' },
+      ],
+      title: '',
+      description: '',
     }
+  },
+  methods: {
+    setTitle(e: any) {
+      this.title = e.target.value
+    },
+    setDescription(e: any) {
+      this.description = e.target.value
+    },
+    submitHandler() {
+      this.list.push({
+        _id: Date.now(),
+        title: this.title,
+        description: this.description,
+      })
+
+      this.title = ''
+      this.description = ''
+    },
   },
 }
 </script>
 
-<template></template>
+<template>
+  <form @submit.stop.prevent="submitHandler">
+    <input type="text" v-bind:value="title" @change="setTitle" />
+    <input type="text" v-bind:value="description" @change="setDescription" />
+    <button type="submit">add</button>
+  </form>
+  <div class="list" v-for="item of list">
+    <div class="title">{{ item.title }}</div>
+    <div class="description">{{ item.description }}</div>
+  </div>
+</template>
 
 <style scoped>
 * {
@@ -16,7 +51,7 @@ export default {
   margin: 0;
 }
 
-.root {
+.list {
   display: flex;
 }
 </style>
---
+++ src/components/Input.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      value: '', // состояние для Input
+    }
+  },
+  methods: {
+    setValue(e) {
+      this.value = e.target.value
+    },
+  },
+}
+</script>
+<template>
+  <input type="text" @change="title = $event.currentTarget.value" !:value="value" />
+</template>
+
+<style scoped></style>
