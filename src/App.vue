4294be431c780416f8a753b580f4da0a9f74aab6
nardo88 <nardo1988@mail.ru>
2026-02-21 17:58:44 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      total: 0, // Значение в состоянии
+    }
+  },
+}
+</script>
+
+<template></template>
+
+<style scoped>
+* {
+  padding: 0;
+  margin: 0;
+}
+
+.root {
+  display: flex;
+}
+</style>
